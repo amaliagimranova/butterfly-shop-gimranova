31f94b5f3504fb19fe361e0a4b9e99c9219fe22d
Amalia Gimranova <amaliagimranova@yandex.ru>
2022-08-11 09:08:26 +0300

    Верстка и использование JS для Контакты и FAQ

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,10 +1,47 @@
 $(document).ready(function(){
+
+  // Меню в мобильной версии
+  $('.js-header-burger').on('click', function(e) {
+    e.preventDefault();
+    $('.js-header-burger').toggleClass('open');
+    $('.js-menu').slideToggle();
+  });
+
+  // Галерея отзывов
   $('.js-reviews-slick').slick({
     dots: false
   });
 
+  // Табы в контактах
+  $('.js-tabs-link').on('click', function(e) {
+    e.preventDefault();
+
+    $('.js-tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    // Смена контента
+    let index = $(this).index('.js-tabs-link');
+
+    $('.js-tabs-content').removeClass('active');
+    $('.js-tabs-content').eq(index).addClass('active');
+  });
+
+  // FAQ
+  let prevStatus;
+  $('.js-faq-btn').on('click', function() {
+
+    if (this === prevStatus) {
+      $(this).next().slideToggle();
+    } else {
+      $('.js-faq-btn').next().slideUp();
+      $(this).next().slideDown();
+    }
+    prevStatus = this;
 
 
+
+
+  })
 
 
 
